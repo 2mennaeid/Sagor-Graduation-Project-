bf2a23a9f2699265a743c1cefa1d92c3730024a0
Menna Eid <90302774+2mennaeid@users.noreply.github.com>
2024-07-07 18:55:24 +0300

    Delete Debug directory

--- Debug/Src/SYSTICK_Prog.d
+++
@@ -1,9 +0,0 @@
-Src/SYSTICK_Prog.o: ../Src/SYSTICK_Prog.c ../Inc/BIT_MATH.h \
- ../Inc/STDTYPES.h ../Inc/stm32f103xx.h ../Inc/errorstate.h \
- ../Inc/SYSTICK_Priv.h ../Inc/SYSTICK_Interface.h
-../Inc/BIT_MATH.h:
-../Inc/STDTYPES.h:
-../Inc/stm32f103xx.h:
-../Inc/errorstate.h:
-../Inc/SYSTICK_Priv.h:
-../Inc/SYSTICK_Interface.h:
